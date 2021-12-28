e8833ed5a84afe1744ee87710b01703641cb229b
Samuel74028 <samuel.yenlin@gmail.com>
2021-12-28 14:43:29 -0800

    Practice Day 1

---
+++ app.js
@@ -0,0 +1,40 @@
+let firstName = "Sam";
+let lastName = "Lee";
+let fullName = `${firstName} ${lastName}`;
+//String Method
+//let index = fullName, index('u');
+//let lastindex = fullName.lastIndexOf("u");
+//console.log(fullName.slice(0, index));
+//console.log(fullName.substr(0, 6));
+//let result = fullName.replace("a", "w");
+const radius = 10;
+const pi = 3.14;
+let area = pi * radius ** 2;
+console.log(area);
+let age = 16;
+console.log(`My name is ${fullName} and my age is ${age}.`);
+console.log(`My first name has ${firstName.length} letters.`);
+
+//Array Method
+let names = ["James", "Curry", "Chris"];
+console.log(names[2]);
+
+/*
+let join = names.join(",");
+let index = names.indexOf("Chris");
+let concat = names.concat(["Samuel", "Vincent"]);
+Concat method don't alter the original array
+let push = names.push("Joe");
+push method does altar the original array
+let pop = names.pop();
+print out the last value inside an array
+*/
+
+//let search = fullName.includes("S");
+//This return a boolean
+
+let score = "100";
+score = Number(score);
+//the score is being converted from string to number
+let type = typeof score;
+console.log(type);
